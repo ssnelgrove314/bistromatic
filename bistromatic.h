a4bda43a2d40e95bd7a0f15f6776021c86961c37
Scott Snelgrove <ssnelgro@e1z2r7p30.42.us.org>
2018-07-10 14:20:21 -0700

    I like vscode a lot

--- bistromatic.h
+++ bistromatic.h
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-char *bistromatic(char *input, char *base, int size);
+char *bistromatic(char *input, char *base, size_t size);
+long ft_atol_base_str(char *input, char *base, size_t size);
+char *ft_ltoa_base_str(char *input, char *base, size_t size);
 
 #endif
